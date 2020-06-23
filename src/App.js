f32cdd8af3967b62adc592cda39284d492fbe097
asfand987 <asfand987.ak@gmail.com>
2020-06-23 16:03:54 +0100

    Added covid-19 api for all countries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import CardDeck from "react-bootstrap/CardDeck"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+    .get("https://disease.sh/v2/countries")
+    .then(res => {
+      console.log(res.data);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}, []);
+
   return (
     <div> 
       <CardDeck>
-    <Card bg ="secondary" text="white" className="text-center">
+    <Card bg ="secondary" text="white" className="text-center" style = {{margin: "10px"}}>
     <Card.Body>
       <Card.Title>Cases</Card.Title>
       <Card.Text>
@@ -18,7 +30,7 @@ function App() {
       <small>Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card bg="danger" text="white" className="text-center">
+  <Card bg="danger" text="white" className="text-center" style = {{margin: "10px"}}>
     <Card.Body>
       <Card.Title>Deaths</Card.Title>
       <Card.Text>
@@ -29,7 +41,7 @@ function App() {
       <small>Last updated 3 mins ago</small>
     </Card.Footer>
   </Card>
-  <Card bg="success" text= "white" className="text-center">
+  <Card bg="success" text= "white" className="text-center" style = {{margin: "10px"}}>
     <Card.Body>
       <Card.Title>Recoveries</Card.Title>
       <Card.Text>
