bb7a89d8ed52d6d7f547a4b254e58ce2dbb6fb9d
asfand987 <asfand987.ak@gmail.com>
2020-06-26 16:01:43 +0100

    Updates

---
+++ precache-manifest.3af9ef022598e52cc83e0cffaf572247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cea3e2ba240fef5ec1351744596144e",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "fe4a80b797ff0c90e2d8",
+    "url": "/Covid19-Tracker/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f62bc595e4733baf1afc",
+    "url": "/Covid19-Tracker/static/css/main.ed043337.chunk.css"
+  },
+  {
+    "revision": "fe4a80b797ff0c90e2d8",
+    "url": "/Covid19-Tracker/static/js/2.d8a59a07.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Covid19-Tracker/static/js/2.d8a59a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f62bc595e4733baf1afc",
+    "url": "/Covid19-Tracker/static/js/main.a8796a58.chunk.js"
+  },
+  {
+    "revision": "93d9623381c78dc76ce3",
+    "url": "/Covid19-Tracker/static/js/runtime-main.c7913f75.js"
+  }
+]);
